a249873324588a4ce7f5da33e70ca86f44a9e0bc
orchlonn <ch.orchlon@gmail.com>
2024-10-27 20:52:32 -0700

    reduced the video resolution using ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffmpeg = require("fluent-ffmpeg");
 
 const app = express();
-const port = 8001;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req: any, res: any) => {
+  // Get the path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send("Bad request: Missing file path");
+  }
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", function () {
+      console.log("Processing finished successfully");
+      res.status(200).send("Processing finished successfully");
+    })
+    .on("error", function (err: any) {
+      console.log("An error occurred: " + err.message);
+      res.status(500).send("An error occurred: " + err.message);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("Video processing service is now running");
+  console.log(`Server is running on port ${port}`);
 });
